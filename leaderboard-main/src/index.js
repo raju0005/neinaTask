904a006608e4225cbe6ee435634587ca63b45695
raju0005 <anderajesh15@gmail.com>
2024-10-20 18:52:54 +0530

    cors

--- leaderboard-main/src/index.js
+++ leaderboard-main/src/index.js
@@ -8,11 +8,7 @@ import user from "./routes/user.routes.js";
 const app = express();
 
 // Use CORS middleware to allow requests from specified origins
-app.use(cors({
-  origin:'https://neina-task.vercel.app/'
-  
-}
-));
+app.use(cors());
 
 // Middlewares
 app.use(express.json());
@@ -21,31 +17,31 @@ app.use(cookieParser());
 
 // Define your routes after the middleware
 app.get("/api/auth", (req, res) => {
-    res.json("I'm coming from backend");
+  res.json("I'm coming from backend");
 });
 app.use("/api/auth/v1", auth);
 app.use("/api/user/v1", user);
 
 // For wrong APIs
 app.use((req, res) => {
-    res.status(404).json({
-        success: false,
-        message: "Route not found. Please check the URL and try again.",
-    });
+  res.status(404).json({
+    success: false,
+    message: "Route not found. Please check the URL and try again.",
+  });
 });
 
 // Error handling middleware (optional)
 app.use((err, req, res, next) => {
-    console.error(err);
-    res.status(500).json({
-        success: false,
-        message: "Internal server error.",
-        error: err.message,
-    });
+  console.error(err);
+  res.status(500).json({
+    success: false,
+    message: "Internal server error.",
+    error: err.message,
+  });
 });
 await connectDb();
 
 app.listen(7000, async () => {
-    console.log("Server is running on port 7000");
-    
+  console.log("Server is running on port 7000");
+
 });
